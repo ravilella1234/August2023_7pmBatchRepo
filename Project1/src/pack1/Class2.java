3db776a5e0ef58c43d9fd2b55828779ea3806564
ravilella1234 <masters.selenium@gmail.com>
2023-08-22 19:58:47 +0530

    Different Projects inheritance

---
+++ Project1/src/pack1/Class2.java
@@ -0,0 +1,19 @@
+package pack1;
+
+public class Class2 extends Class1
+{
+
+	public Class2(int a, int b) 
+	{
+		super(a, b);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		Class2 c2 = new Class2(10, 20);
+		c2.add();
+		c2.sub();
+	}
+
+}
